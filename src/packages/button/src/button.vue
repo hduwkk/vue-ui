3f62ebf49e1d63a18f7e9860b44c38f44f95e564
steve <873057692@qq.com>
2018-08-29 23:21:02 +0800

    ....

---
+++ src/packages/button/src/button.vue
@@ -0,0 +1,62 @@
+<template>
+<button
+  class="ep-button"
+  @click="handleClick"
+  :disabled="buttonDisabled || loading"
+  :autofocus="autofocus"
+  :type="nativeType"
+  :class="[
+    type ? 'ep-button--' + type : '',
+    buttonSize ? 'ep-button--' + buttonSize : '',
+    {
+      'is-disabled': buttonDisabled,
+      'is-loading': loading,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle
+    }
+  ]">
+    <i class="fa fa-spinner" v-if="loading"></i>
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
+</button>
+</template>
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    },
+    size: String,
+    icon: {
+      type: String,
+      default: ''
+    },
+    nativeType: {
+      type: String,
+      default: 'button'
+    },
+    loading: Boolean,
+    disabled: Boolean,
+    plain: Boolean,
+    autofocus: Boolean,
+    round: Boolean,
+    circle: Boolean
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit('click', evt)
+    }
+  },
+  computed: {
+    buttonSize() {
+      return this.size || this._elFormItemSize
+    },
+    buttonDisabled() {
+      return this.disabled
+    }
+  }
+}
+</script>
